caeb660bde20cddcbd6050895ea91843b74745ee
Michael Dupuis Jr <michael.dupuis@dockyard.com>
2013-09-13 11:14:31 -0400

    Outlines methods to test for

--- test/finders_test.rb
+++ test/finders_test.rb
@@ -4,80 +4,42 @@ require 'capybara_hunter'
 describe '.string' do
   let(:post) { Post.new(1, 'The fourth post title', 'Vivamus porttitor augue quis nibh ultrices, sed venenatis sem sollicitudin. Cras gravida tortor neque, quis lobortis nunc ultrices vitae. Pellentesque.', author: 'Juan Doe') }
   let(:string) { Capybara.string TestString }
-  let(:unique) { 'Adipiscing' }
-  let(:duplicate) { 'Suspendisse' }
+  let(:unique) { 'Jane Doe' }
+  let(:multi_in_table) { 'John Doe' }
+  let(:multi_in_tables) { ' id ' }
 
-  describe '#find_' do
-    it 'finds a row when passed a unique string' do
+  describe '#find_row' do
+    it 'finds a unique row when passed a string' do
       string.find_row(unique).text.must_have_content unique
-      string.find_row(unique).text.wont_have_content duplicate
+      string.find_row(unique).text.wont_have_content multi_in_table
     end
-
-    it 'finds a table when passed a unique string' do
-      string.find_table(unique).text.must_have_content 'Adipiscing'
-      string.find_table(unique).text.wont_have_content duplicate
-    end
-
-    describe 'passing an instance' do
-      it 'finds the post row of when passed an instance' do
-        string.find_row(post).text.must_have_content 'Edit unique'
-        #string.find(string.row(post)).text.must_have_content 'Edit unique'
-        #string.find(string.row(post)).text.wont_have_content 'Cats'
-      end
-    end
-
-    #describe 'passing a hash' do
-      #it 'finds a unique element when passed its class' do
-        #string.find_row(class: 'animal').text.must_have_content 'unique'
-      #end
-
-      #it 'finds the first root vegetable row when passed multiple classes via hash' do
-        #string.first(string.row({ class: 'vegetable.root' })).text.must_have_content 'Carrots'
-        #string.first(string.row({ class: 'vegetable.root' })).text.wont_have_content 'unique'
-        #string.first(string.row({ class: 'vegetable.root' })).text.wont_have_content 'duplicate'
-      #end
-
-       #it 'finds the first root vegetable row when passed a single id via hash' do
-         #string.first(string.row({ id: 'vegetable_1' })).text.must_have_content 'duplicate'
-         #string.first(string.row({ id: 'vegetable_1' })).text.wont_have_content 'Carrots'
-       #end
-
-       #it 'finds the row with class vegetable and content peppers' do
-         #string.find(*string.row({ class: 'vegetable', content: 'Peppers' })).text.must_have_content 'Peppers'
-         #string.find(*string.row({ class: 'vegetable', content: 'Peppers' })).text.wont_have_content 'duplicate'
-       #end
-    #end
+  # When passed a string/object for a row that is unique on the page, find_row returns the row
+  #
+  # When passed a string/object for a row that is unique to a table, find_row returns the row 
+  # if nested in a within block for the table
+  #
+  # When passed an object for a row that is built without an id or class, find_row will not
+  # return any results
+  #
+  # When passed a string for a row that is not unique, find_row will return 'ambiguous'
   end
 
-  #describe '#table' do
-    #it 'finds the table of vegetable types when passed a string that appears within the table' do
-      #string.find(*string.table('duplicate')).text.must_have_content 'duplicate'
-      #string.find(*string.table('duplicate')).text.wont_have_content 'unique'
-    #end
+  describe '#find_table' do
+    # When passed a string that appears once on the page, find_table returns
+    # the table.
+    it 'finds a table when passed a unique string' do
+      string.find('table', text: unique).text.must_have_content unique
+      string.find('table', text: unique).text.wont_have_content multi_in_table
+    end
 
-    #describe 'passing a hash' do
-      #it 'finds the table of animals when passed a single class via hash' do
-        #string.find(string.table({ class: 'animals' })).text.must_have_content 'unique'
-        #string.find(string.table({ class: 'animals' })).text.wont_have_content 'duplicate'
-      #end
+    # When passed a string that appears multiple times in a single table, find_table
+    # returns the table
+    it 'finds a table when passed a string that appears multiple times in a single table' do
+      string.find('table', text: multi_in_table).text.must_have_content multi_in_table
+      string.find('table', text: multi_in_table).text.wont_have_content unique
+    end
 
-      #it 'finds the table of root vegetable when passed a multiple classes via hash' do
-        #string.find(string.table({ class: 'vegetables.root' })).text.must_have_content 'Carrots'
-        #string.find(string.table({ class: 'vegetables.root' })).text.wont_have_content 'unique'
-        #string.find(string.table({ class: 'vegetables.root' })).text.wont_have_content 'duplicate'
-      #end
-
-       #it 'finds the table of root vegetable when passed a single id via hash' do
-         #string.find(string.table({ id: 'root-vegetables' })).text.must_have_content 'Carrots'
-         #string.find(string.table({ id: 'root-vegetables' })).text.wont_have_content 'unique'
-       #end
-    #end
-
-    #describe 'passing an instance' do
-      #it 'finds the table of animal categories when passed an instance' do
-        #string.find(string.table(post)).text.must_have_content 'unique'
-        #string.find(string.table(post)).text.wont_have_content 'Cats'
-      #end
-    #end
-  #end
+    # When passed a string that appear in multiple tables, find_table return
+    # 'ambiguous match'
+  end
 end
--- test/string.rb
+++ test/string.rb
@@ -1,30 +1,19 @@
 TestString =  <<-STRING
-  <h1>All posts</h1>
+  <h1>Posts by John</h1>
   <table>
     <thead>
       <tr>
+        <th>Author</th>
         <th>Title</th>
         <th>Body</th>
         <th colspan=3>Actions</th>
       </tr>
     </thead>
-
     <tbody>
-      <tr class="post" id="post_1">
-    <td>The first post title</td>
-    <td>Suspendisse id purus quis augue consequat varius ut eget diam. Donec ut lacus vestibulum, porttitor nulla porta, convallis ligula. Nunc sodales venenatis sapien sit amet consequat.</td>
-    <td><a href="/posts/1">Show</a></td>
-    <td><a href="/posts/1/edit">Edit</a></td>
-    <td><a data-confirm="Are you sure?" data-method="delete" href="/posts/1" rel="nofollow">Destroy</a></td>
-  </tr><tr class="post" id="post_2">
-    <td>The second post title</td>
-    <td>Donec iaculis ultricies orci id consequat. Etiam varius dignissim tincidunt. Sed in lacinia augue. Proin ullamcorper quam at tincidunt consectetur.</td>
-    <td><a href="/posts/2">Show</a></td>
-    <td><a href="/posts/2/edit">Edit</a></td>
-    <td><a data-confirm="Are you sure?" data-method="delete" href="/posts/2" rel="nofollow">Destroy</a></td>
-  </tr><tr class="post" id="post_3">
+      <tr class="post" id="post_3">
+    <td>John Doe</td>
     <td>The third post title</td>
-    <td>Adipiscing quam. Donec posuere condimentum dolor, mollis consectetur diam consectetur eu. Etiam ipsum augue, imperdiet ac porttitor tristique, hendrerit.</td>
+    <td>Sed id tortor odio. Proin a adipiscing quam. Donec posuere condimentum dolor, mollis consectetur diam consectetur eu. Etiam ipsum augue, imperdiet ac porttitor tristique, hendrerit.</td>
     <td><a href="/posts/3">Show</a></td>
     <td><a href="/posts/3/edit">Edit</a></td>
     <td><a data-confirm="Are you sure?" data-method="delete" href="/posts/3" rel="nofollow">Destroy</a></td>
@@ -36,20 +25,22 @@ TestString =  <<-STRING
   <table>
     <thead>
       <tr>
+        <th>Author</th>
         <th>Title</th>
         <th>Body</th>
         <th colspan=3>Actions</th>
       </tr>
     </thead>
-
     <tbody>
       <tr class="post" id="post_1">
+    <td>Jane Doe</td>
     <td>The first post title</td>
     <td>Suspendisse id purus quis augue consequat varius ut eget diam. Donec ut lacus vestibulum, porttitor nulla porta, convallis ligula. Nunc sodales venenatis sapien sit amet consequat.</td>
     <td><a href="/posts/1">Show</a></td>
     <td><a href="/posts/1/edit">Edit</a></td>
     <td><a data-confirm="Are you sure?" data-method="delete" href="/posts/1" rel="nofollow">Destroy</a></td>
   </tr><tr class="post" id="post_2">
+    <td>Jane Doe</td>
     <td>The second post title</td>
     <td>Donec iaculis ultricies orci id consequat. Etiam varius dignissim tincidunt. Sed in lacinia augue. Proin ullamcorper quam at tincidunt consectetur.</td>
     <td><a href="/posts/2">Show</a></td>
