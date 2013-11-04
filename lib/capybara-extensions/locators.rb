4e3b03fe2c6f2986d3db0b4e1d8d72130b4f4472
Michael Dupuis Jr <michael.dupuis@dockyard.com>
2013-11-04 10:29:31 -0500

    Adds YARD comments to Locators.

--- lib/capybara-extensions/locators.rb
+++ lib/capybara-extensions/locators.rb
@@ -1,6 +1,11 @@
 # Locators for xpath.
 module CapybaraExtensions::Locators
 
+  # Builds a locator via the given src or alt.
+  #
+  # @param options[Hash] must pass a hash containing src and/or alt to match against.
+  # @return [String] string formatted for finding an attribute with xpath.
+  #
   def image_locator(options)
     locator = String.new
     locator.concat "[@alt='#{options[:alt]}']" if options[:alt]
@@ -8,6 +13,12 @@ module CapybaraExtensions::Locators
     locator
   end
 
+  # Builds a locator via the given name and content.
+  #
+  # @params name[String] the name of the meta tag sought.
+  # @params content [String] the content of the meta tag sought.
+  # @return [String] string formatted for finding a meta tag with xpath.
+  #
   def meta_tag_locator(name, content)
     locator = String.new
     locator.concat "[@name='#{name}']"
